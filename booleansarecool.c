1632dd5748b6d272c979b7a69927085b756557e0
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-04-08 21:53:15 -0700

    boolean

---
+++ booleansarecool.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>  // Import the boolean header
+/*
+If 1 then true if 0 then false. Wow I fucking comment like a program. I need to get some sleep
+#include <sleep.h>
+goddamn
+*/
+int main() {
+  bool doIneedsleep = true;
+  bool haveIhadany = false;
+  printf("%d\n", doIneedsleep);
+  printf("%d", haveIhadany);
+  
+  return 0;
+}
