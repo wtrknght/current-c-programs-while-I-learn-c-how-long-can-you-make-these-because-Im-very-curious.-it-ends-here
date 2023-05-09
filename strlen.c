e555a8ef8af6bc4d4184522a79ef953fc363c004
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-05-09 14:15:30 -0400

    Update strlen.c

--- strlen.c
+++ strlen.c
@@ -6,9 +6,11 @@ int main(){
     char a[]="Shit";
     char b[20]={'P','r','o','g','r','a','m','s','\0'};
     //\0 is null btw
-    // using the %zu format specifier to print size_t
-    printf("Length of string a = %zu \n",strlen(a));
-    printf("Length of string b = %zu \n",strlen(b));
-    sleep(5)
+    // using the %d to print int
+    int c = strlen(a);
+    int d = strlen(b);
+    printf("Length of string a = %d \n", c);
+    printf("Length of string b = %d \n", d);
+    sleep(5);
     return 0;
 }
